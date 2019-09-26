b1e0f06a2d2ad266737622c0aa301f6816b74ff8
YONGSU JEONG <jos50275266@gmail.com>
2019-09-27 07:05:34 +0900

    OS Module을 이용해 Performance Data 가져오기 part-1
    1. os.freemem()
    2. os.totalmem()
    3. os.type()
    4. os.uptime()
    5. cpus[0].model
    6. cpus.length
    7. cpus[0].speed

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -1,4 +1,35 @@
-//  The node program that captures local performance data
+//  The node program that captures local performance data and sends it up to the socket.io server
 // Req:
 // - farmhash
 // - socket.io-client
+
+const os = require("os");
+const cpus = os.cpus();
+// What do we need to know from node about performace?
+// - CPU load (current)
+// - Memory Usage
+//  - free
+const freeMem = os.freemem();
+//  - total
+const totalMem = os.totalmem();
+//  - usedMem
+const usedMem = totalMem - freeMem;
+// memUsage
+const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
+// - OS type
+const osType = os.type(); // Windows_NT
+// - uptime
+// uptime 이란 서버가 가동된 시간
+// 재부팅이 없이 연속적으로 서버가 다운없이 지속된 시간을 말합니다.
+const upTime = os.uptime();
+
+// - CPU info
+// os.cpus() returns an array of objects containing information about each logical CPU core
+// core * 2
+//   - Type
+const cpuModel = cpus[0].model;
+//   - Number of Cores:
+const numCores = cpus.length;
+//   - Clock Speed:
+const cpuSpeed = cpus[0].speed;
+// Model as well as speed will be the same no matter what which core is
