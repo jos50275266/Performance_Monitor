6a98db72e46c58243b770c4fdd7173343ab917c3
jos50275266 <jos50275266@gmail.com>
2019-09-29 00:52:35 +0900

    Clock Ticking Part - 1
    index.js (socket-io - client) 에서 clientAuth Event로 전송한 Key 값을
    이용해 valid 한 client인지 검증하고 valid 하다면 clients Room에 Join
    시킨다 UI user 또한 동일하다, 만일 검증되지 않는다면 해당 socket의
    연결을 끊는다. 그리고 검증된 경우 바로 index.js 에서 perfData Event로
    emit 한 데이터를 받아서 console.log(data)로 1초 간격으로 출력해준다.
    
    그리고 redis의 경우 redis-cli 를 실행후 monitor를 하면 1초 간격으로
    출력시 발생하는 pub/sub stufff를 얻는다. 즉, redis로 부터 chatter를
    얻는다.

--- server/socketMain.js
+++ server/socketMain.js
@@ -1,5 +1,23 @@
 function socketMain(io, socket) {
-  console.log("A socket connected!", socket.id);
+  //   console.log("A socket connected!", socket.id);
+
+  // What type of client is joining with this room. We can now communicate with just the node clients
+  socket.on("clientAuth", key => {
+    if (key === "5afkopskfpefkpo3123") {
+      // valid nodeClient
+      socket.join("clients");
+    } else if ((key = "4mpamklamfklsam0")) {
+      // valid ui client has joined
+      socket.join("ui");
+    } else {
+      // an invalid client has joined. GoodBye
+      socket.disconnect(true);
+    }
+  });
+
+  socket.on("perfData", data => {
+    console.log(data);
+  });
 }
 
 module.exports = socketMain;
