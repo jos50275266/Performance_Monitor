a16076d2dd6bfe03aec73f532f26beaf19ce7762
jos50275266 <jos50275266@gmail.com>
2019-09-28 13:09:53 +0900

    PerformanceData, cpuAverage, getCpuLoad 함수 작성
    용어 정리:
    
    Sys: The CPU is running kernal code. This includes device drivers and
    kernel modules.
    
    User: The CPU is running code in user-mode. This includes your
    application code. Note that if an application tries to read from disk or
    write to network, it acutally goes to sleep while the kernel performs
    that work, and wakes up the application again
    
    Nice: The user code can be executed in "normal" priority, or various
    degrees of "below normal" priority. You can, for example, run some kind
    of report generation process at a lower priority and interactive
    processes at normal priority. Nice is when CPU is executing a user tak
    having below-normal priority.
    
    I/O Wait: Sometimes the CPU has only one thing to do - wait for the
    results of a disk/network read/write. This isn't as uncommon as you'd
    think. A file server, for example, would nearly spend all i's life
    waiting for disk reads and network writes to complete. I/O Wait is when
    the CPU is waiting for an I/O Operation to complete, and the CPU can't
    be used for anything else
    
    Ideal: And when there is really nothing the kernel can do, it just as to
    waste away this slice of time. Technically, when runnable queue is empty
    and there are no I/O Operations going on, the CPU usage is marked as
    idle
    
    함수 설명:
    
    cpuAverage:
    os.cpus() method를 실행하면, an array of object containing information
    about each logical CPU core를 return 하는데 이 object의 종류는:
    model (string), speed (number),
    times <object> : user, nice, sys, idle, irq
    
    os.cpus() 가 return 한 값을 cpus 변수에 할당하고, forEach를 통해 각
    CPU를 가져오고 그 안에 for in 문을 이용해 모든 times object
    properties의 총합의 경우 totalMs 변수에 할당하고, times object의 idle
    값을 따로 분리해 idleMs 변수에 할당해 모든 CPU의 totlMs 값과 idleMs 값을
    구한후 이값을 cpus.length(cpu 갯수)로 나눔으로써 현재 컴퓨터의 CPU의
    totalMs and idleMs 평균을 object 형태로 return 한다.
    
    getCpuLoad():
    setTimeout을 이용하기 때문에 비동기로 함수를 작성해 start 변수에 cpuAverage()를 한 번
    실행시켜 할당하고, setTimeout 함수에서 1 초 후에 cpuAverage()를 한번 더
    실행시켜 그 값을 end 에 할당하고 start 변수의 end and start idle and
    total 값을 각각 구해서 그 차이를 idleDifference and totalDifference
    변수에 각각 할당하고, 백분위로 CPU 사용도를 확인하기위해 percentageCpu
    에 100 * idleDifference / totalDifference를 하고 Promise 를 return 하기
    때문에 console.log or return 대신에 resolve 함수를 percentagCpu 값을
    전달해준다.
    
    performanceData():
    이 함수에는 freeMem, totalMem, usedMem, osType, cpuModel, numCores,
    cpuSpeed와 같은 모든 정보를 Nodejs os module을 이용해 가져온 후
    할당하고, cpuLoad 값의 경우 이전에 생성한 getCpuLoad 함수를 이용해
    비동기로 가져온다. getCpuLoad 함수는 비동기 함수이기때문에 동일하기
    performanceData 함수 또한 return promise를 하고 최종적으로 resolve를
    이용해 결과값을 return해준다.

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -2,34 +2,99 @@
 // Req:
 // - farmhash
 // - socket.io-client
-
 const os = require("os");
-const cpus = os.cpus();
-// What do we need to know from node about performace?
-// - CPU load (current)
-// - Memory Usage
-//  - free
-const freeMem = os.freemem();
-//  - total
-const totalMem = os.totalmem();
-//  - usedMem
-const usedMem = totalMem - freeMem;
-// memUsage
-const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
-// - OS type
-const osType = os.type(); // Windows_NT
-// - uptime
-// uptime 이란 서버가 가동된 시간
-// 재부팅이 없이 연속적으로 서버가 다운없이 지속된 시간을 말합니다.
-const upTime = os.uptime();
+function performanceData() {
+  return new Promise(async (resolve, reject) => {
+    const cpus = os.cpus();
+    // What do we need to know from node about performace?
+    // - CPU load (current)
+    // - Memory Usage
+    //  - free
+    const freeMem = os.freemem();
+    //  - total
+    const totalMem = os.totalmem();
+    //  - usedMem
+    const usedMem = totalMem - freeMem;
+    // memUsage
+    const memUsage = Math.floor((usedMem / totalMem) * 100) / 100;
+    // - OS type
+    const osType = os.type(); // Windows_NT
+    // - uptime
+    // uptime 이란 서버가 가동된 시간
+    // 재부팅이 없이 연속적으로 서버가 다운없이 지속된 시간을 말합니다.
+    const upTime = os.uptime();
 
-// - CPU info
-// os.cpus() returns an array of objects containing information about each logical CPU core
-// core * 2
-//   - Type
-const cpuModel = cpus[0].model;
-//   - Number of Cores:
-const numCores = cpus.length;
-//   - Clock Speed:
-const cpuSpeed = cpus[0].speed;
-// Model as well as speed will be the same no matter what which core is
+    // - CPU info
+    // os.cpus() returns an array of objects containing information about each logical CPU core
+    // core * 2
+    //   - Type
+    const cpuModel = cpus[0].model;
+    //   - Number of Cores:
+    const numCores = cpus.length;
+    //   - Clock Speed:
+    const cpuSpeed = cpus[0].speed;
+    // Model as well as speed will be the same no matter what which core is
+
+    const cpuLoad = await getCpuLoad();
+    resolve({
+      freeMem,
+      totalMem,
+      usedMem,
+      osType,
+      upTime,
+      cpuModel,
+      numCores,
+      cpuSpeed,
+      cpuLoad
+    });
+  });
+}
+
+// CPUs is all numCores, we need the average of all the cores which will give us the CPU average
+function cpuAverage() {
+  // Everytime cpuAverage is called, it will refresh this cpu data.
+  const cpus = os.cpus();
+  // Get Milliseconds each mode, But this number is since reboot
+  // so get it now, and git it in 100ms and compare
+  let idleMs = 0;
+  let totalMs = 0;
+  // Loop Through Each Core
+  cpus.forEach(aCore => {
+    // Loop Through Each Property of the current core
+    for (type in aCore.times) {
+      //   console.log(type);
+      totalMs += aCore.times[type];
+    }
+
+    idleMs += aCore.times.idle;
+  });
+
+  return {
+    idle: idleMs / cpus.length,
+    total: totalMs / cpus.length
+  };
+}
+
+// Because the times property is time since boot, We will get now times,
+// and 100ms from now times Compare them, that will give us current load
+// This is asynchronou function because there is a setTimeout. Let's return new Promise
+// to make this function as an asynchronou function.
+function getCpuLoad() {
+  return new Promise((resolve, reject) => {
+    const start = cpuAverage();
+    setTimeout(() => {
+      const end = cpuAverage();
+      const idleDifference = end.idle - start.idle;
+      const totalDifference = end.total - start.total;
+      // console.log(idleDifference, totalDifference);
+      // To calculate the % of used CPU
+      const percentageCpu =
+        100 - Math.floor((100 * idleDifference) / totalDifference);
+      resolve(percentageCpu);
+    }, 100);
+  });
+}
+
+performanceData().then(allPerformanceData => {
+  console.log(allPerformanceData);
+});
